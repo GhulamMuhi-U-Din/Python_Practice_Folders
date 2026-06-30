e7c2e71a2e0e590380937d2dd761e59217de7961
GhulamMuhi-U-Din <92mohiudin@gmail.com>
2026-06-30 21:51:42 +0500

    added abstraction

---
+++ Classes & Objects/abstraction.py
@@ -0,0 +1,25 @@
+# abstraction in classes 
+
+class Person:
+
+    def __init__(self, name):
+        self.name = name
+
+    def display(self):
+        print("Name:", self.name)
+
+
+class Student(Person):
+
+    def __init__(self, name, age):
+        super().__init__(name)
+        self.age = age
+
+    def display(self):
+        super().display()
+        print("Age :", self.age)
+
+
+student = Student("Ali", 20)
+
+student.display()

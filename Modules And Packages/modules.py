3a3e8d6e67561ec6031718df008a410f4cb02596
GhulamMuhi-U-Din <92mohiudin@gmail.com>
2026-06-30 19:49:32 +0500

    added modules.py

---
+++ Modules And Packages/modules.py
@@ -0,0 +1,26 @@
+# basic modules
+
+# math
+
+import math
+
+print(math.pi)
+print(math.sqrt(64))
+
+# random
+
+import random
+
+print(random.randint(1, 10))
+
+# os
+
+import os
+
+print(os.getcwd())
+
+# date time
+
+from datetime import datetime
+
+print(datetime.now())

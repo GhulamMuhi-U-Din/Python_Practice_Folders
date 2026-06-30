bd9138341442936baa76db9ad28004a9700a70aa
GhulamMuhi-U-Din <92mohiudin@gmail.com>
2026-06-30 18:48:14 +0500

    added built-in_functions.py

---
+++ Functions/built-in_functions.py
@@ -0,0 +1,82 @@
+# len() function to get the length
+
+name = "John snow"
+print(len(name)) # length of  string
+
+# length of list
+
+numbers = [100, 200, 300, 400, 500]
+print(len(numbers))
+
+# length of tuple
+
+numbers = (10, 20, 30)
+print(len(numbers))
+
+# lengh of dictionary
+
+student = {
+    "name": "Ali",
+    "age": 20
+}
+
+print(len(student))
+
+# sum() function
+
+marks = [15, 35, 65, 72, 41, 89]
+print(sum(marks))
+
+# min() function to get minimum value
+
+marks = [15, 35, 65, 72, 41, 89]
+print(min(marks))
+
+#  max() function to get maximum value
+
+marks = [15, 35, 65, 72, 41, 89]
+print(max(marks))
+
+# sorted() function to sort elements in a list
+
+marks = [15, 35, 65, 72, 41, 89]
+print(sorted(marks))
+
+# abs() funtion to get absolute value
+
+print(abs(-39))
+
+# round() function toget roundoff value
+
+print(round(4.2))
+
+# pow() function to get power pf a digit
+
+print(pow(2, 4)) # 2 raised to power 4
+
+# type() function to get the data type of variable
+
+x = 10
+print(type(x))
+
+name = "john"
+print(type(name))
+
+# isinstance() funtion to know if the data type is true
+
+x = 50
+print(isinstance(x, int))
+
+# id() functon to get the memory address
+
+x = 50
+print(id(x))
+
+# dir() function to ge the available methods of a functions
+
+name = "Python"
+print(dir(name))
+
+# help() fuction to get info
+
+help(len)

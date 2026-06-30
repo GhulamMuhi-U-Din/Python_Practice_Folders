d78742cfc99a5e4259a5921d01b9e23ab5a5d94c
GhulamMuhi-U-Din <92mohiudin@gmail.com>
2026-06-30 19:04:54 +0500

    added singleAndMultipleExceptions.py

---
+++ Exception Handling/singleAndMultipleExceptions.py
@@ -0,0 +1,28 @@
+# single exception case
+
+print("Start")
+
+try:
+    print(10 / 0)
+
+except:
+    print("An error occurred.")
+
+print("End")
+
+# multiple exception case
+
+try:
+    number = int(input("Enter number: "))
+
+    print(100 / number)
+
+except ValueError:
+    print("Invalid number.")
+
+except ZeroDivisionError:
+    print("Cannot divide by zero.")
+
+print("End")
+
+

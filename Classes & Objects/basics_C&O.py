eb0e4282bf178c90dcf17d43b66707c350f0d690
GhulamMuhi-U-Din <92mohiudin@gmail.com>
2026-06-30 21:38:28 +0500

    added basics_C&O.py

---
+++ Classes & Objects/basics_C&O.py
@@ -0,0 +1,32 @@
+class Student:
+
+    def __init__(self, name, age):
+        self.Name = name
+        self.Age = age
+
+    def display(self):
+        print(self.Name)
+        print(self.Age)
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+s1 = Student(name, age)
+s2 = Student(name, age)
+
+s1.display()
+s2.display()
+
+# different way of accessing variables
+
+class Student:
+
+    def __init__(self):
+        self.name = "Ali"
+        self.age = 20
+
+
+student1 = Student()
+
+print(student1.name)
+print(student1.age)

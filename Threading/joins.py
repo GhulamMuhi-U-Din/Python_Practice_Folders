1379b339eab0b9e763cd955602d95ade8a5eb4fa
GhulamMuhi-U-Din <92mohiudin@gmail.com>
2026-07-09 16:58:50 +0500

    modified joins.py

--- Threading/joins.py
+++ Threading/joins.py
@@ -21,3 +21,19 @@ t1.start()
 t1.join()
 
 print("Program Ended")
+
+# another example
+
+import threading
+
+def print_numbers():
+    for i in range(1, 6):
+        print(i)
+
+t = threading.Thread(target=print_numbers)
+
+t.start()
+
+t.join()
+
+print("Done")

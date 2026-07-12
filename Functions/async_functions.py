d724589296e90d9c680e8d9bbd95fbf153f9e957
GhulamMuhi-U-Din <92mohiudin@gmail.com>
2026-07-12 17:23:55 +0500

    modified async_functions.py

--- Functions/async_functions.py
+++ Functions/async_functions.py
@@ -1,3 +1,7 @@
+# =====================================
+# SINGLE TASK
+#======================================
+
 # Import asyncio module
 import asyncio
 
@@ -16,3 +20,34 @@ async def hello():
 
 # Start and run the async program
 asyncio.run(hello())
+
+# =====================================
+# MULTIPLE TASKS
+#======================================
+
+import asyncio
+
+async def task1():
+
+    print("Task 1 Started")
+
+    await asyncio.sleep(3)
+
+    print("Task 1 Completed")
+
+async def task2():
+
+    print("Task 2 Started")
+
+    await asyncio.sleep(1)
+
+    print("Task 2 Completed")
+
+async def main():
+
+    await asyncio.gather(
+        task1(),
+        task2()
+    )
+
+asyncio.run(main())

58069f7a278ba3d49e01c163321d1f898839c1ff
GhulamMuhi-U-Din <92mohiudin@gmail.com>
2026-07-17 18:09:02 +0500

    added student.py

---
+++ Student Management System/student.py
@@ -0,0 +1,42 @@
+# Student class
+class Student:
+
+    # Class variable
+    total_students = 0
+
+    # Constructor
+    def __init__(self, name, age):
+
+        self.name = name
+        self._age = age
+
+        Student.total_students += 1
+
+    # Property
+    @property
+    def age(self):
+        return self._age
+
+    # Setter
+    @age.setter
+    def age(self, value):
+
+        if value > 0:
+            self._age = value
+
+    # String representation
+    def __str__(self):
+
+        return f"Name: {self.name}, Age: {self.age}"
+
+    # Class method
+    @classmethod
+    def total(cls):
+
+        return cls.total_students
+
+    # Static method
+    @staticmethod
+    def school():
+
+        return "GMD School"
